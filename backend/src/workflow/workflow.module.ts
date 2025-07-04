fe21bba07cd32169ffcdc26e20a301b4b4a5f439
Mukesh Kumar Singh <mukeshkumarsingh@Mukeshs-MacBook-Air.local>
2025-07-04 14:48:34 +0530

    fix: import AuditLogModule in WorkflowModule to resolve AuditLogService dependency

--- backend/src/workflow/workflow.module.ts
+++ backend/src/workflow/workflow.module.ts
@@ -5,9 +5,15 @@ import { WorkflowService } from './workflow.service';
 import { WorkflowSyncService } from './workflow-sync.service';
 import { PrismaModule } from '../prisma/prisma.module';
 import { UserModule } from '../user/user.module';
+import { AuditLogModule } from '../audit-log/audit-log.module';
 
 @Module({
-  imports: [PrismaModule, UserModule, ScheduleModule.forRoot()],
+  imports: [
+    PrismaModule,
+    UserModule,
+    AuditLogModule,
+    ScheduleModule.forRoot()
+  ],
   controllers: [WorkflowController],
   providers: [WorkflowService, WorkflowSyncService],
   exports: [WorkflowService],
