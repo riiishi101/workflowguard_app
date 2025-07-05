857445af5d28f6d6b0d46b9f059344426d313b65
Mukesh Kumar Singh <mukeshkumarsingh@Mukeshs-MacBook-Air.local>
2025-07-05 09:57:34 +0530

    Simplify configurations for Vercel deployment

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,23 +3,11 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
-  server: {
-    host: "::",
-    port: 8080,
-  },
+export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  build: {
-    outDir: "dist",
-    rollupOptions: {
-      output: {
-        manualChunks: undefined,
-      },
-    },
-  },
-}));
+});
