10a7361af9564fb5b2f45978d77f4e165f48caa8
riiishi101 <raijungrishi@gmail.com>
2025-07-07 23:27:11 +0530

    Remove duplicate workflowguard_app-main folder

--- workflowguard_app-main/backend/prisma/migrations/20250703164356_add_hubspot_tokens_to_user/migration.sql
+++
@@ -1,4 +0,0 @@
--- AlterTable
-ALTER TABLE "User" ADD COLUMN     "hubspotAccessToken" TEXT,
-ADD COLUMN     "hubspotRefreshToken" TEXT,
-ADD COLUMN     "hubspotTokenExpiresAt" TIMESTAMP(3);
--- workflowguard_app-main/backend/src/workflow/dto/index.ts
+++
@@ -1,2 +0,0 @@
-export * from './create-workflow.dto';
-export * from './update-workflow.dto'; 
