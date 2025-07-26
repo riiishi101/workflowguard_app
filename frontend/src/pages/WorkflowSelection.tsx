cfa92ab4fca0c536ec05bdd10a6655f68359be80
riiishi101 <raijungrishi@gmail.com>
2025-07-26 13:30:34 +0530

    Remove 'in console' debug text from WorkflowSelection info box

--- frontend/src/pages/WorkflowSelection.tsx
+++ frontend/src/pages/WorkflowSelection.tsx
@@ -231,7 +231,7 @@ const WorkflowSelection = () => {
               Your trial includes Professional Plan features, allowing you to
               monitor up to 500 workflows and retain 90 days of history. Get
               started by selecting your critical workflows below.
-            </p>in console
+            </p>
           </div>
         </div>
 
